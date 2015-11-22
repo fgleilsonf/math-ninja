5d56f84fd0adb261f4d8347f51fbe52f5b64e283
Gleilson Ferreira <Gleilson Ferreira>
2015-11-22 18:37:59 -0200

    Ajustes

--- main.lua
+++ main.lua
@@ -8,11 +8,9 @@ display.setStatusBar (display.HiddenStatusBar)
 
 local composer = require( "composer" )
 
- composer.gotoScene( "credits", {time=800, effect="crossFade"} )
-
---if (hasFirstAccess() == 0) then
---    setFirstAccess(1)
---    composer.gotoScene( "tutorial", {time=800, effect="crossFade"} )
---else
---    composer.gotoScene( "menu", {time=800, effect="crossFade"} )
---end
+if (hasFirstAccess() == 0) then
+    setFirstAccess(1)
+    composer.gotoScene( "tutorial", {time=800, effect="crossFade"} )
+else
+    composer.gotoScene( "menu", {time=800, effect="crossFade"} )
+end
